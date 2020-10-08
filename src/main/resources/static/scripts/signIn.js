7e09a5caf9a6da4c003176a4d89c5dd976e58c5a
unknown <cmeide@uark.edu>
2020-10-08 16:14:25 -0500

    View and Validation for Sign In page

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -3,6 +3,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function validateForm() {
-	// TODO: Validate the user input
+	if (empID.value === undefined || empID.value === "") {
+		console.log("not valid b/c id is blank");
+		return false;
+	}
+
+	if (isNaN(empID.value)) {
+		console.log("not valid b/c id is not a number");
+		return false;
+	}
+
+	if (pass.value === undefined || pass.value === "") {
+		console.log("not valid b/c pass is blank");
+		return false;
+	}
+	console.log("valid");
 	return true;
 }
