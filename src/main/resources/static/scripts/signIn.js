230aa6bdfe07e0533b1e2999dbc6c65c62cfb2a1
unknown <cmeide@uark.edu>
2020-10-10 12:28:26 -0500

    Removed unneccessary code

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -4,22 +4,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function validateForm() {
 	if (empID.value === undefined || empID.value === "") {
-		// console.log("not valid b/c id is blank");
 		displayError("The id is not valid because it is blank");
 		return false;
 	}
 
 	if (isNaN(empID.value)) {
-		// console.log("not valid b/c id is not a number");
 		displayError("The id is not valid because it is not at number");
 		return false;
 	}
 
 	if (pass.value === undefined || pass.value === "") {
-		// console.log("not valid b/c pass is blank");
 		displayError("The password is not valid because it is blank");
 		return false;
 	}
-	console.log("valid");
 	return true;
 }
